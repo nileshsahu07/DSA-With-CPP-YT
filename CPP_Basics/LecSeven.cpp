6f504dfd6da78783bc56cd6e2510ed96e7a54acb
nileshhackingly07 <nilesh.pancholi@hackingly.in>
2026-01-09 17:33:36 +0530

    bitwise operators

---
+++ CPP_Basics/LecSeven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//Bitwise Operator
+
+int main(){
+    int a = 4, b = 8;
+
+    cout<<(a & b)<<endl;
+    cout<<(a | b)<<endl;
+    cout<<(a ^ b)<<endl;
+
+    cout<<(10<<2)<<endl;
+    cout<<(10>>1)<<endl;
+
+    return 0;
+}
